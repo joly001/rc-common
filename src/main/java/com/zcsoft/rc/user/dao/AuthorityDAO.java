b77f4aae04cd3bc4ac87d0a2fb6f5936c5eb2cae
joly001 <joly_huang@163.com>
2019-04-02 16:58:45 +0800

    init

--- src/main/java/com/zcsoft/rc/user/dao/AuthorityDAO.java
+++ src/main/java/com/zcsoft/rc/user/dao/AuthorityDAO.java
@@ -4,7 +4,16 @@ package com.zcsoft.rc.user.dao;
 import com.zcsoft.rc.user.model.entity.Authority;
 import com.zcsoft.rc.common.dao.BaseDAO;
 
+import java.util.List;
+
 
 public interface AuthorityDAO extends BaseDAO<Authority,java.lang.String> {
+
+    /**
+     * 根据用户id查询权限
+     * @param userId
+     * @return
+     */
+    List<Authority> queryListByUserId(String userId);
 
 }
--- src/main/java/com/zcsoft/rc/user/dao/impl/AuthorityDAOImpl.java
+++ src/main/java/com/zcsoft/rc/user/dao/impl/AuthorityDAOImpl.java
@@ -8,8 +8,14 @@ import com.zcsoft.rc.user.model.entity.Authority;
 import com.zcsoft.rc.user.dao.AuthorityDAO;
 import com.zcsoft.rc.common.dao.impl.BaseDAOImpl;
 
+import java.util.List;
+
 
 @Repository
 public class AuthorityDAOImpl extends BaseDAOImpl<Authority,java.lang.String> implements AuthorityDAO {
-	
+
+    @Override
+    public List<Authority> queryListByUserId(String userId) {
+        return selectList("queryListByUserId", userId);
+    }
 }
