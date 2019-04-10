52b2d62b80f1c8d40b3b7be7d15c7ca4ccdb561d
joly001 <joly_huang@163.com>
2019-04-10 16:37:12 +0800

    init

--- src/main/java/com/zcsoft/rc/mileage/dao/WorkSegmentDAO.java
+++ src/main/java/com/zcsoft/rc/mileage/dao/WorkSegmentDAO.java
@@ -4,6 +4,9 @@ package com.zcsoft.rc.mileage.dao;
 import com.zcsoft.rc.mileage.model.entity.WorkSegment;
 import com.zcsoft.rc.common.dao.BaseDAO;
 
+import java.util.Date;
+import java.util.List;
+
 
 public interface WorkSegmentDAO extends BaseDAO<WorkSegment,java.lang.String> {
 
@@ -13,5 +16,13 @@ public interface WorkSegmentDAO extends BaseDAO<WorkSegment,java.lang.String> {
      * @return
      */
     WorkSegment queryByStartLongitudeEndLongitude(double longitude);
+
+    /**
+     * 根据时间查询作业面列表
+     * @param startWorkDate
+     * @param endWorkDate
+     * @return
+     */
+    List<WorkSegment> queryListByWorkDate(Date startWorkDate, Date endWorkDate);
 
 }
--- src/main/java/com/zcsoft/rc/mileage/dao/impl/WorkSegmentDAOImpl.java
+++ src/main/java/com/zcsoft/rc/mileage/dao/impl/WorkSegmentDAOImpl.java
@@ -8,6 +8,11 @@ import com.zcsoft.rc.mileage.model.entity.WorkSegment;
 import com.zcsoft.rc.mileage.dao.WorkSegmentDAO;
 import com.zcsoft.rc.common.dao.impl.BaseDAOImpl;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 @Repository
 public class WorkSegmentDAOImpl extends BaseDAOImpl<WorkSegment,java.lang.String> implements WorkSegmentDAO {
@@ -15,6 +20,15 @@ public class WorkSegmentDAOImpl extends BaseDAOImpl<WorkSegment,java.lang.String
     @Override
     public WorkSegment queryByStartLongitudeEndLongitude(double longitude) {
         return selectOne("queryByStartLongitudeEndLongitude", longitude);
+    }
+
+    @Override
+    public List<WorkSegment> queryListByWorkDate(Date startWorkDate, Date endWorkDate) {
+        Map<String, Object> parameter = new HashMap<>();
+        parameter.put("startWorkDate", startWorkDate);
+        parameter.put("endWorkDate", endWorkDate);
+
+        return selectList("queryListByWorkDate", parameter);
     }
 
 }
