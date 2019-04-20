5d55075b0f6462d969b8ef5e24d4debb34c7572a
joly001 <joly_huang@163.com>
2019-04-20 17:20:01 +0800

    init

--- src/main/java/com/zcsoft/rc/mileage/dao/MileageSegmentDAO.java
+++ src/main/java/com/zcsoft/rc/mileage/dao/MileageSegmentDAO.java
@@ -7,4 +7,11 @@ import com.zcsoft.rc.common.dao.BaseDAO;
 
 public interface MileageSegmentDAO extends BaseDAO<MileageSegment,java.lang.String> {
 
+    /**
+     * 根据坐标查询
+     * @param longitude
+     * @return
+     */
+    MileageSegment queryByStartLongitudeEndLongitude(double longitude);
+
 }
--- src/main/java/com/zcsoft/rc/mileage/dao/impl/MileageSegmentDAOImpl.java
+++ src/main/java/com/zcsoft/rc/mileage/dao/impl/MileageSegmentDAOImpl.java
@@ -11,5 +11,9 @@ import com.zcsoft.rc.common.dao.impl.BaseDAOImpl;
 
 @Repository
 public class MileageSegmentDAOImpl extends BaseDAOImpl<MileageSegment,java.lang.String> implements MileageSegmentDAO {
-	
+
+    @Override
+    public MileageSegment queryByStartLongitudeEndLongitude(double longitude) {
+        return selectOne("queryByStartLongitudeEndLongitude", longitude);
+    }
 }
