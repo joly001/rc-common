3a0538b01afdf7d5587af0f6a589fd0a3652d1dd
joly001 <joly_huang@163.com>
2019-08-12 16:43:13 +0800

    init

--- src/main/resources/sql/4_INIT_MONGODB_DATA.sql
+++ src/main/resources/sql/4_INIT_MONGODB_DATA.sql
@@ -2,3 +2,7 @@ db.rc.cordon.ensureIndex({"geometry" : "2dsphere"})
 db.rc.cable.ensureIndex({"geometry" : "2dsphere"})
 db.rc.cablePolygon.ensureIndex({"geometry" : "2dsphere"})
 db.rc.safetyZone.ensureIndex({"geometry" : "2dsphere"})
+
+./mongoimport -h localhost:28118 -d rc -c rc.cable ./test.json
+
+./mongoimport -h localhost:28118 -d rc -c rc.cordon ./test.json
