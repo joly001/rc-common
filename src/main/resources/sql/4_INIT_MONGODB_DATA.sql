4e757324f5536b18f01ae1ee2a5602e14f62e34d
joly <JolyEmail@Gmail.COM>
2019-09-03 16:33:59 +0800

    init

--- src/main/resources/sql/4_INIT_MONGODB_DATA.sql
+++ src/main/resources/sql/4_INIT_MONGODB_DATA.sql
@@ -3,6 +3,6 @@ db.rc.cable.ensureIndex({"geometry" : "2dsphere"})
 db.rc.cablePolygon.ensureIndex({"geometry" : "2dsphere"})
 db.rc.safetyZone.ensureIndex({"geometry" : "2dsphere"})
 
-./mongoimport -h localhost:28118 -d rc -c rc.cable ./test.json
+./mongoimport -h localhost:28118 -d rc -c rc.cable ./cable.json
 
-./mongoimport -h localhost:28118 -d rc -c rc.cordon ./test.json
+./mongoimport -h localhost:28118 -d rc -c rc.cordon ./cordon.json
